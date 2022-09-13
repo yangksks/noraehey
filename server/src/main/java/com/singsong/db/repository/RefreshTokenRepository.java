360dce5e0f6af0acdb516e40fcdf572d7422823f
김혜성 <syong0716@naver.com>
2022-09-13 16:43:38 +0900

    mysql server fix

--- server/src/main/java/com/singsong/db/repository/RefreshTokenRepository.java
+++ server/src/main/java/com/singsong/db/repository/RefreshTokenRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-    Optional<RefreshToken> findByMemberId(Long memberId);
+    Optional<RefreshToken> findByMemberMemberId(Long memberId);
 }
