94882e8066cf72c972f6d1c47ffb750e96369d91
정민호 <etminho12@naver.com>
2022-09-15 15:40:06 +0900

    add swagger config

--- server/src/main/java/com/singsong/api/controller/MemberController.java
+++ server/src/main/java/com/singsong/api/controller/MemberController.java
@@ -1,7 +1,11 @@
 package com.singsong.api.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/member")
 public class MemberController {
+
+    
 }
