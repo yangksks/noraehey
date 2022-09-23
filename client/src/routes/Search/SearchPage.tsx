7cc2d8ef56f30e47fba236d8797c6403a2f18973
yb8350 <yb8350@naver.com>
2022-09-23 15:07:31 +0900

    Feat: 검색 페이지 개발

--- client/src/routes/Search/SearchPage.tsx
+++ client/src/routes/Search/SearchPage.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from 'react';
+import styled from 'styled-components';
+import Container from '../../style/style';
+import { fetchData } from '../../utils/api/api';
 
 const SearchPage = () => {
-  return (
-    <div>SearchPage</div>
-  )
-}
+  const [searchText, setSearchText] = useState('');
+  const [searchList, setSearchList] = useState([]);
 
-export default SearchPage
+  return (
+    <Container>
+      <SearchForm>
+        <form
+          onSubmit={() => {
+            fetchData
+              .get('/api/v1/song/search?word=피어나&page=0')
+              .then((res) => {
+                setSearchList(res.data.songEntityResList);
+              });
+          }}>
+          <input
+            type="text"
+            onChange={({ target: { value } }) => {
+              setSearchText(value);
+            }}
+          />
+        </form>
+      </SearchForm>
+      <SearchResult></SearchResult>
+    </Container>
+  );
+};
+
+const SearchForm = styled.div``;
+const SearchResult = styled.div``;
+
+export default SearchPage;
