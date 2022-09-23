4dea53f7a6dcd2a2178ca534a683894b3e722f10
김혜성 <syong0716@naver.com>
2022-09-23 15:44:59 +0900

    Feat: 노래 가수명으로 검색 추가

--- server/src/main/java/com/singsong/api/controller/SongController.java
+++ server/src/main/java/com/singsong/api/controller/SongController.java
@@ -46,27 +46,32 @@ public class SongController {
     SongLevelService songLevelService;
 
     @GetMapping("/search")
-    public ResponseEntity<SongListRes> searchSongList(@RequestParam("word") String word, @RequestParam("page") int page){
+    public ResponseEntity<SongListRes> searchSongList(@RequestParam("key") int key, @RequestParam("word") String word, @RequestParam("page") int page) {
         int pageSize = 20;
         boolean hasMore = false;
         List<SongEntityRes> songEntityResList = new ArrayList<>();
-        List<Song> songList = songService.searchSongList(word);
+        List<Song> songList = new ArrayList<>();
+        if (key == 1) {
+            songList = songService.searchSongListByTitle(word);
+        } else {
+            songList = songService.searchSongListBySinger(word);
+        }
         int fromIdx = page * pageSize;
         int toIdx = (page + 1) * pageSize - 1 >= songList.size() ? songList.size() - 1 : (page + 1) * pageSize - 1;
-        if(songList.size() <= fromIdx || page < 0){
+        if (songList.size() <= fromIdx || page < 0) {
             return ResponseEntity.status(200).body(SongListRes.of(hasMore, songEntityResList));
         }
         List<Song> songListPaged = songList.subList(fromIdx, toIdx + 1);
-        for(Song song : songListPaged){
+        for (Song song : songListPaged) {
             SongEntityRes songEntityRes = SongEntityRes.of(song);
             songEntityResList.add(songEntityRes);
         }
-        if(songList.size() > toIdx + 1) hasMore = true;
+        if (songList.size() > toIdx + 1) hasMore = true;
         return ResponseEntity.status(200).body(SongListRes.of(hasMore, songEntityResList));
     }
 
     @GetMapping("/info/{songId}")
-    public ResponseEntity<SongDetailRes> getSongDetail(@PathVariable("songId") Long songId, @ApiIgnore Authentication authentication){
+    public ResponseEntity<SongDetailRes> getSongDetail(@PathVariable("songId") Long songId, @ApiIgnore Authentication authentication) {
         Member member = jwtAuthenticationUtil.jwtTokenAuth(authentication);
         Long memberId = member.getMemberId();
         Song song = songService.getSongBySongId(songId);
@@ -76,7 +81,7 @@ public class SongController {
     }
 
     @PostMapping("/level")
-    public ResponseEntity<? extends BaseResponseBody> evaluateSongLevel(@RequestBody SongLevelPostReq songLevelPostReq, @ApiIgnore Authentication authentication){
+    public ResponseEntity<? extends BaseResponseBody> evaluateSongLevel(@RequestBody SongLevelPostReq songLevelPostReq, @ApiIgnore Authentication authentication) {
         Member member = jwtAuthenticationUtil.jwtTokenAuth(authentication);
         Song song = songService.getSongBySongId(songLevelPostReq.getSongId());
         int updatedSongLevel = songLevelService.evaluateSongLevel(member, song, songLevelPostReq.getSongLevel());
@@ -86,7 +91,7 @@ public class SongController {
     }
 
     @PostMapping("/like")
-    public ResponseEntity<? extends BaseResponseBody> registerSongLike(@RequestBody Map<String, Long> songLikeMap, @ApiIgnore Authentication authentication){
+    public ResponseEntity<? extends BaseResponseBody> registerSongLike(@RequestBody Map<String, Long> songLikeMap, @ApiIgnore Authentication authentication) {
         Member member = jwtAuthenticationUtil.jwtTokenAuth(authentication);
         Song song = songService.getSongBySongId(songLikeMap.get("songId"));
         int result = songLikeService.registerSongLike(member, song);
@@ -95,7 +100,7 @@ public class SongController {
     }
 
     @DeleteMapping("/like")
-    public ResponseEntity<? extends BaseResponseBody> deleteSongLike(@RequestBody Map<String, Long> songLikeMap, @ApiIgnore Authentication authentication){
+    public ResponseEntity<? extends BaseResponseBody> deleteSongLike(@RequestBody Map<String, Long> songLikeMap, @ApiIgnore Authentication authentication) {
         Member member = jwtAuthenticationUtil.jwtTokenAuth(authentication);
         Song song = songService.getSongBySongId(songLikeMap.get("songId"));
         int result = songLikeService.deleteSongLike(member, song);
@@ -104,7 +109,7 @@ public class SongController {
     }
 
     @GetMapping("/like")
-    public ResponseEntity<SongListRes> getSongLikeList(@RequestParam("page") int page, @ApiIgnore Authentication authentication){
+    public ResponseEntity<SongListRes> getSongLikeList(@RequestParam("page") int page, @ApiIgnore Authentication authentication) {
         Member member = jwtAuthenticationUtil.jwtTokenAuth(authentication);
         int pageSize = 20;
         Pageable pageable = PageRequest.of(page, pageSize);
@@ -113,7 +118,7 @@ public class SongController {
         List<Song> songLikeList = songLikeService.getSongLikeListByMemberId(member.getMemberId(), pageable);
         List<Song> hasMoreList = songLikeService.getSongLikeListByMemberId(member.getMemberId(), PageRequest.of(page + 1, pageSize));
         if (hasMoreList.size() != 0) hasMore = true;
-        for(Song song : songLikeList){
+        for (Song song : songLikeList) {
             SongEntityRes songEntityRes = SongEntityRes.of(song);
             songEntityResList.add(songEntityRes);
         }
