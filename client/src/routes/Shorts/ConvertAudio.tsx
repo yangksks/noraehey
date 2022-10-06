2031ac7c397932924e8ef9f07a91127ec7adbc99
원재호 <wonjh07@naver.com>
2022-10-07 01:18:45 +0900

    fix: 매거진 id fix

--- client/src/routes/Shorts/ConvertAudio.tsx
+++ client/src/routes/Shorts/ConvertAudio.tsx
@@ -56,8 +56,8 @@ const ConvertAudio = (props: ConvertType) => {
       } else {
         setMessage('업로드에 실패했습니다.');
       }
-    } catch(err) {
-      console.log(err)
+    } catch (err) {
+      console.log(err);
       alert('ios 및 일부기기에서 편집이 제한됩니다. 원본을 업로드합니다.');
       props.getConvertAudio(m4a);
     }
